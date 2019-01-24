e5c9ac44f0e428cde990178785f875fa30a235da
Givary <givaryutha18@gmail.com>
2019-01-24 09:56:29 +0700

    Tugas Menghitung Keliling Bujur Sangkar dan Lingkaran

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello World!");
+        System.out.println("Menghitung Keliling Bujur Sangkar dan Lingkaran");
+        BujurSangkar bujunrsangkar = new BujurSangkar();
+        Lingkaran lingkaran = new Lingkaran();
+        bujunrsangkar.hitungkelilingbujursangkar(8);
+        lingkaran.hitungkelilinglingkaran(12);
     }
 }
