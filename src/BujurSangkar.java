c85f2f4a38435afa46c8b05bc74c909f039f224b
Givary <givaryutha18@gmail.com>
2019-01-24 10:01:26 +0700

    Tugas Menghitung Keliling bentuk

---
+++ src/BujurSangkar.java
@@ -0,0 +1,9 @@
+public class BujurSangkar {
+    private int keliling;
+
+    public void hitungkelilingbujursangkar(int sisi){
+        keliling = 4*sisi;
+        System.out.println("Keliling Bujur Sangkar"+sisi+"Adalah"+keliling);
+
+    }
+}
---
+++ src/Lingkaran.java
@@ -0,0 +1,10 @@
+public class Lingkaran {
+   private double phi = 3.14;
+   public double keliling;
+
+   public void hitungkelilinglingkaran(int jari){
+       keliling = 2*phi*jari;
+       System.out.println("Keliling Lingkaran"+jari+"Adalah"+keliling);
+
+   }
+}
